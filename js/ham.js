4576c7b487a6aaca754d8ea0adadd6b674e725a6
gmikadze <73239601+gmikadze@users.noreply.github.com>
2020-12-02 15:50:04 +0400

    draft

---
+++ js/ham.js
@@ -0,0 +1,18 @@
+var menus = document.getElementsByClassName('hamburger-menu');
+
+[].forEach.call(menus, function (m) {
+    m.addEventListener('click', function () {
+        m.classList.toggle('open');
+    });
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50vh";
+  }
+  prevScrollpos = currentScrollPos;
+}
